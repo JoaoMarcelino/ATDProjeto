d97930068dd930b5a2da67a7baadc922830bc351
DrSh1ny <francisco201262@gmail.com>
2020-05-11 22:26:33 +0100

    transformada de fourier em janelas

---
+++ janelaFourier.m
@@ -0,0 +1,38 @@
+function [frequencias,arrayJanelas] = janelaFourier(atividade,fs,tempoJanela,tempoSobreposicao,modo)
+    Tframe = tempoJanela; % largura da janela de análise em s
+    Toverlap = tempoSobreposicao; % sobreposiçao das janelas em s
+    Nframe = round(Tframe*fs); % número de amostras na janela
+    Noverlap = round(Toverlap*fs); % número de amostras sobrepostas na janela
+    
+    if(modo==1)
+        h = hamming(Nframe); % janela de hamming
+    end
+    
+    if mod(Nframe, 2)==0
+        f_frame = -fs/2:fs/Nframe:fs/2-fs/Nframe;
+    else 
+        f_frame = -fs/2+fs/(2*Nframe):fs/Nframe:fs/2-fs/(2*Nframe);
+    end
+    
+    nframes = 0; % para guardar
+    frames=[];
+    
+    for ii = 1:Nframe-Noverlap:N-Nframe
+        % aplicar a janela ao sinal do tempo
+        if(modo==1)
+            x_frame = atividade(ii:ii+Nframe-1).*h;
+        end
+        
+        % obter a magnitude da fft do sinal
+        m_X_frame = abs(fftshift(fft(x_frame)));
+
+
+        nframes = nframes+1;
+
+        frames=[frames, m_X_frame];
+    end
+    
+    frequencias=f_frame;
+    arrayJanelas=frames;
+end
+
---
+++ plotAllActivities.m
@@ -0,0 +1,18 @@
+function [outputArg1] = plotAllActivities(dados,fs)
+    
+    for i=1:12
+        subplot(12,3,i*3-2);   %eixo X
+        [frequencias,amplitude]=transFourierDiscreta(dados{1}{i}{3},5);
+        plot(frequencias,amplitude);
+        
+        subplot(12,3,i*3-1);   %eixo Y
+        [frequencias,amplitude]=transFourierDiscreta(dados{3}{i}{3},5);
+        plot(frequencias,amplitude);
+        
+        subplot(12,3,i*3);   %eixo Z
+        [frequencias,amplitude]=transFourierDiscreta(dados{3}{i}{3},5);
+        plot(frequencias,amplitude);
+        
+    end
+end
+
