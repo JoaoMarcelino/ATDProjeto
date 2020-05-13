000f38cf628f2a6b1e43f2f84fa3c93b263c5aec
DrSh1ny <francisco201262@gmail.com>
2020-05-13 22:48:38 +0100

    funcao gostosa

--- calcularFrequenciaAndar.m
+++ calcularFrequenciaAndar.m
@@ -1,24 +1,25 @@
-function [media,desvioPadrao] = calcularFrequenciaAndar(dados,fs,eixo)
+function [media,desvioPadrao] = calcularFrequenciaAndar(dados,fs,atividade,eixo)
     arrayFrequencias=[];
     media=0;
     desvioPadrao=0;
     
-    for i=1:length(dados{eixo}{1}) %percorrer todos os segmentos de "Walking" no eixo X
+    for i=1:length(dados{eixo}{atividade}) %percorrer todos os segmentos de "Walking" no eixo X
         
-        segmento=dados{eixo}{1}{i};
+        segmento=dados{eixo}{atividade}{i};
         segmento=detrend(segmento);
         
         [freq,amp]=transFourierDiscreta(segmento,fs);
         plot(freq,amp);
         [maximo,indice]=max(amp);
         
-        arrayFrequencias=[arrayFrequencias, abs(freq(indice))];
-        
-        
+        arrayFrequencias=[arrayFrequencias, abs(freq(indice))*60];
+           
     end
+    
+    
     
     media=mean(arrayFrequencias);
     desvioPadrao=std(arrayFrequencias);
-    spm=(1/media)*60;
+    
 end
 
--- mediaFrequenciasAtividade.m
+++ mediaFrequenciasAtividade.m
@@ -1,11 +1,34 @@
-function [mediaFrequencias] = mediaFrequenciasAtividade(dados,atividade,fs)
-    media=[]
+function [binsFrequencias] = mediaFrequenciasAtividade(dados,fs,atividade,eixo,binWidth)
+    %1-transformar cada segmento de uma atividade
+    %2-percorrer os valores das frequencias obtidas e coloca-los no balde
+    %de frequencia correspondente
+    %profit??
     
-    for i=1:length(dados{1}{atividade})
-        segmento=dados{1}{atividade}{i};
+    
+    binsFrequencias=[-fs/2:binWidth:fs/2-binWidth,1]; %baldes e frequencias
+    binsFrequencias=binsFrequencias';   
+    binsFrequencias(1:end,2)=0;
+    for i=1:length(dados{eixo}{atividade})            
+        segmento=dados{eixo}{atividade}{i};
+        segmento=detrend(segmento);
         [freq,amp]=transFourierDiscreta(segmento,fs);
+        for j=1:length(freq)
+            frequencia=freq(j);
+            amplitude=amp(j);
+            
+            
+            for k=1:length(binsFrequencias)-1
+                if(frequencia>=binsFrequencias(k) && frequencia<binsFrequencias(k+1))
+                    binFreq=binsFrequencias(k,1);
+                    valor=binsFrequencias(k,2);
+                    binsFrequencias(k,2)=valor+amplitude;
+                    break;
+                end
+            end
+        end
+        
     end
-
-
+        
+    
 end
 
